80244a0c57041dbdd568337065e91f3c6b7a06e1
Philip Rosenberg-Watt <p.rosenberg-watt@cablelabs.com>
2018-07-28 14:59:23 -0600

    Add custom debug level
    Faster and eaiser than modifiying global AppDaemon logging output level.

--- ape.py
+++ ape.py
@@ -22,8 +22,10 @@ class Event:
 
 class Monkey(hass.Hass):
 
+    DEBUG_LEVEL="INFO"
+
     def initialize(self):
-        self.log("initialize()", level="DEBUG")
+        self.log("initialize()", level=Monkey.DEBUG_LEVEL)
         self.log("args: {0}".format(self.args), level="INFO")
 
         if "occupancy_state" in self.args \
@@ -34,6 +36,7 @@ class Monkey(hass.Hass):
                 self.events_db = self.args["events_db"]
 
             self.events = self.load(self.events_db)
+            self.log("post event-load", level=Monkey.DEBUG_LEVEL)
             if self.events is None:
                 self.log("No events pickle file found, starting from scratch.", level="WARNING")
                 self.forget(None, None, None)
@@ -61,7 +64,7 @@ class Monkey(hass.Hass):
             self.log("Missing required parameter(s). Cannot continue.", level="ERROR")
 
     def decide(self, entity, attribute, old, new, kwargs):
-        self.log("decide({0}, {1}, {2}, {3}, {4})".format(entity, attribute, old, new, kwargs), level="DEBUG")
+        self.log("decide({0}, {1}, {2}, {3}, {4})".format(entity, attribute, old, new, kwargs), level=Monkey.DEBUG_LEVEL)
 
         if new == 'on':
             # cancel all scheduled "do" callbacks
@@ -80,7 +83,7 @@ class Monkey(hass.Hass):
             self.log("{0} is {1}, nothing to see or do".format(self.args["occupancy_state"], new))
 
     def start_doing(self, kwargs):
-        self.log("start_doing({0})".format(kwargs), level="DEBUG")
+        self.log("start_doing({0})".format(kwargs), level=Monkey.DEBUG_LEVEL)
 
         # stop observing
         self.watching = False
@@ -96,7 +99,7 @@ class Monkey(hass.Hass):
         self.do_handles.append(h)
 
     def monkey_see(self, entity, attribute, old, new, kwargs):
-        self.log("monkey_see({0}, {1}, {2}, {3}, {4})".format(entity, attribute, old, new, kwargs), level="DEBUG")
+        self.log("monkey_see({0}, {1}, {2}, {3}, {4})".format(entity, attribute, old, new, kwargs), level=Monkey.DEBUG_LEVEL)
 
         if self.watching and new != old:
             self.log("appending event to observations...", level="INFO")
@@ -104,10 +107,10 @@ class Monkey(hass.Hass):
             self.observations.append(e)
 
             self.log("...{0} observation(s)...".format(len(self.observations)), level="INFO")
-            self.log("...{0}".format(self.observations), level="DEBUG")
+            self.log("...{0}".format(self.observations), level=Monkey.DEBUG_LEVEL)
 
     def monkey_do(self, kwargs):
-        self.log("do({0})".format(kwargs), level="DEBUG")
+        self.log("do({0})".format(kwargs), level=Monkey.DEBUG_LEVEL)
 
         evnt = kwargs["evnt"]
         self.log("replaying {0}".format(evnt), level="INFO")
@@ -120,10 +123,10 @@ class Monkey(hass.Hass):
             self.log("\"new\" was neither \"on\" nor \"off\": {0}".format(evnt.new), level="WARNING")
 
     def remember(self):
-        self.log("remember()", level="DEBUG")
+        self.log("remember()", level=Monkey.DEBUG_LEVEL)
 
         self.log("{0} observations to remember...".format(len(self.observations)), level="INFO")
-        self.log("...{0}".format(self.observations), level="DEBUG")
+        self.log("...{0}".format(self.observations), level=Monkey.DEBUG_LEVEL)
 
         days = {}
         for i in range(0, 7):
@@ -132,7 +135,7 @@ class Monkey(hass.Hass):
         for e in self.observations:
             days[e.dt.weekday()].append(e)
 
-        self.log("observations as days: {0}".format(days), level="DEBUG")
+        self.log("observations as days: {0}".format(days), level=Monkey.DEBUG_LEVEL)
 
         for i in range(0, 7):
             try:
@@ -141,7 +144,7 @@ class Monkey(hass.Hass):
                 right = bisect.bisect_right([e.dt.time() for e in self.events[i]], days[i][-1].dt.time())
                 self.events[i] = self.events[i][:left] + days[i] + self.events[i][right:]
                 self.log("...{0} events for {1} now...".format(len(self.events), calendar.day_name[i]), level="INFO")
-                self.log("...{0}".format(self.events[i]), level="DEBUG")
+                self.log("...{0}".format(self.events[i]), level=Monkey.DEBUG_LEVEL)
             except IndexError:
                 self.log("...{0} has no events yet. Skipping.".format(calendar.day_name[i]), level="INFO")
 
@@ -149,7 +152,7 @@ class Monkey(hass.Hass):
         self.observations = []
 
     def schedule_today(self, kwargs):
-        self.log("schedule_today({0})".format(kwargs), level="DEBUG")
+        self.log("schedule_today({0})".format(kwargs), level=Monkey.DEBUG_LEVEL)
 
         today = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
 
@@ -171,7 +174,7 @@ class Monkey(hass.Hass):
         self.log("{0} events for today, {1} scheduled, {2} skipped".format(len(self.events[today.weekday()]), scheduled_events, skipped_events), level="INFO")
 
     def forget(self, event_name, data, kwargs):
-        self.log("forget({0}, {1}, {2})".format(event_name, data, kwargs), level="DEBUG")
+        self.log("forget({0}, {1}, {2})".format(event_name, data, kwargs), level=Monkey.DEBUG_LEVEL)
 
         self.events = {}
         for i in range(0, 7):
@@ -180,27 +183,33 @@ class Monkey(hass.Hass):
         self.save()
 
     def save(self):
-        self.log(msg="save()", level="DEBUG")
+        self.log(msg="save()", level=Monkey.DEBUG_LEVEL)
 
         self.log("saving {0} observations".format(self.len_d(self.events)), level="INFO")
         with open(self.events_db + '.pkl', 'wb') as f:
             pickle.dump(self.events, f, pickle.HIGHEST_PROTOCOL)
 
     def load(self, name):
-        self.log(msg="load({0})".format(name), level="DEBUG")
+        self.log(msg="load({0})".format(name), level=Monkey.DEBUG_LEVEL)
 
         self.log("loading observations", level="INFO")
         try:
             with open(name + '.pkl', 'rb') as f:
+                self.log("loaded", level=Monkey.DEBUG_LEVEL)
                 return pickle.load(f)
-        except FileNotFoundError:
+        except FileNotFoundError as fnfe:
+            self.log("file not found: {0}".format(fnfe), level="WARN")
             return None
+        except EOFError as eofe:
+            self.log("error opening file: {0}".format(eofe), level="ERROR")
+            return None
+
 
     @staticmethod
     def len_d(d):
         return sum([len(d[k]) for k in d.keys()])
 
     def terminate(self):
-        self.log("terminate()", level="DEBUG")
+        self.log("terminate()", level=Monkey.DEBUG_LEVEL)
 
         self.remember()
