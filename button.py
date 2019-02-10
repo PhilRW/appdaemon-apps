6fa7ed92f99405cd14c372d6ac6d70b77e971c50
Philip Rosenberg-Watt <p.rosenberg-watt@cablelabs.com>
2019-02-10 10:16:53 -0700

    Add controlled light mode to button translator

--- button.py
+++ button.py
@@ -14,14 +14,23 @@ class Translator(hass.Hass):
         self.off_ts = None
         self.presses = []
         self.handle = None
+        self.light_mode = False
 
         if "button" in self.args and \
                 "dash" in self.args and \
                 "timeout" in self.args and \
                 "events" in self.args:
             self.listen_state(self.button_listener, self.args["button"])
+            if "light_mode_event" in self.args and \
+                    "lights" in self.args:
+                self.listen_event(self.light_mode_listener, self.args["light_mode_event"])
         else:
             self.log("Missing required settings, doing nothing.", level="WARNING")
+
+    def light_mode_listener(self, event_name, data, kwargs):
+        self.log("light_mode_listener({0}, {1}, {2})".format(event_name, data, kwargs), level=Translator.DEBUG_LEVEL)
+
+        self.light_mode = not self.light_mode
 
     def button_listener(self, entity, attribute, old, new, kwargs):
         self.log("button_listener({0}, {1}, {2}, {3}, {4})".format(entity, attribute, old, new, kwargs), level=Translator.DEBUG_LEVEL)
@@ -30,8 +39,14 @@ class Translator(hass.Hass):
 
         if new == "on":
             self.on_ts = datetime.datetime.now()
+            if self.light_mode:
+                for e in self.args["lights"]:
+                    self.turn_on(e)
         elif new == "off":
             self.off_ts = datetime.datetime.now()
+            if self.light_mode:
+                for e in self.args["lights"]:
+                    self.turn_off(e)
             dur = self.off_ts - self.on_ts
             self.log("Duration: {0}".format(dur), level=Translator.DEBUG_LEVEL)
             self.presses.append(dur)
@@ -52,9 +67,14 @@ class Translator(hass.Hass):
 
         self.log("Pattern: {0}".format(pattern))
 
-        if pattern in self.args["events"]:
+        if self.args["events"] and pattern in self.args["events"]:
             e = self.args["events"][pattern]
-            self.log("Match found, firing event {0}".format(e))
-            self.fire_event(e)
+            self.log("Match found: {0} == {1}".format(pattern, e))
+            if (not self.light_mode) or \
+                    ("light_mode_event" in self.args and e == self.args["light_mode_event"]):
+                self.log("Firing event {0}".format(e))
+                self.fire_event(e)
+            else:
+                self.log("Not firing event because light_mode is {0}".format(self.light_mode))
 
         self.presses = []
--- docs/button_translator.md
+++ docs/button_translator.md
@@ -9,6 +9,10 @@ button_1_listener:
   module: button
   class: Translator
   button: binary_sensor.button_1
+  light_mode_event: TOGGLE_LIGHT_MORSE_CODE_MODE
+  lights:
+    - light.light_1
+    - light.light_2
   dash: 0.2
   timeout: 2
   events:
@@ -17,3 +21,5 @@ button_1_listener:
 ```
 
 `timeout` is the duration after releasing the button that the app consideres the sequence finished and will fire the event (if a match is found).
+
+`light_mode_event` and `lights` are optional but they will switch into a button controlled mode when `light_mode_event` is fired. You can also fire the `light_mode_event` with the button itself and it will serve to toggle the mode and ignore all other events when the mode is on.
